1b441dc89b009b147348705eee7a72b3c008a236
Jen <jenkin.du@gmail.com>
2022-07-24 19:43:07 -0400

    recursion

--- paihuoche.py
+++ paihuoche.py
@@ -3,8 +3,8 @@ import random
 
 sys.setrecursionlimit(2000)
 
-pokerA = []
-pokerB = []
+pokerA = [12,4,5,13,12,12,4,10,11,1,7,6,2,10,6,5,1,10,10,5,13,11,3,6,9,8,11]
+pokerB = [9,9,2,7,2,4,8,4,13,8,5,14,3,1,7,13,2,7,11,3,1,14,6,8,3,9,12]
 huoche = []
 
 count = 0
@@ -21,10 +21,10 @@ def paihuoche(play):
 	global count
 	with open('metrics.csv', 'a') as f:
 		f.write("{0},{1},{2},{3},{4},{5},{6}\n".format(count,len(pokerA),len(pokerB),len(huoche),'-'.join(map(str,pokerA)),'-'.join(map(str,pokerB)),'-'.join(map(str,huoche))))
-	if len(pokerA) == 0:
+	if play == 'A' and len(pokerA) == 0:
 		print(count, "Player B is win!", len(pokerB), pokerB, pokerA, huoche)
 		return
-	elif len(pokerB) == 0:
+	elif play == 'B' and len(pokerB) == 0:
 		print(count, "Player A is win!", len(pokerA), pokerA, pokerB, huoche)
 		return
 	count+=1
@@ -33,24 +33,12 @@ def paihuoche(play):
 		print("A", che, "paihuoche", huoche)
 		if che in huoche:
 			#A tuiche
-			#print("A tuiche", che, "index", huoche.index(che))
 			tui(pokerA, che)
-			#print("huoche", huoche)
-			#print(pokerA, pokerB)
-			#return
 			paihuoche('A')
 		else:
 			huoche.append(che)
 			#B pai
-			che = pokerB.pop(0)
-			print("B", che, "paihuoche", huoche)
-			if che in huoche:
-				#B tuiche
-				tui(pokerB, che)
-				paihuoche('B')
-			else:
-				huoche.append(che)
-				paihuoche(play)
+			paihuoche('B')
 	else:
 		che = pokerB.pop(0)
 		print("B", che, "paihuoche", huoche)
@@ -61,15 +49,7 @@ def paihuoche(play):
 		else:
 			huoche.append(che)
 			#A pai
-			che = pokerA.pop(0)
-			print("A", che, "paihuoche", huoche)
-			if che in huoche:
-				#A tuiche
-				tui(pokerA, che)
-				paihuoche('A')
-			else:
-				huoche.append(che)
-				paihuoche(play)
+			paihuoche('A')
 
 
 if not len(pokerA) and not len(pokerB):
